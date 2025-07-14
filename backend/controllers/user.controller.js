48e3bc498f3a8de93acdfcc134356642f571d998
이재현 <jaehyeon@ijaehyeon-ui-MacBookAir-2.local>
2025-07-14 23:21:59 +0900

    [BE] fix: getMyProfile API

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -16,9 +16,9 @@ const getAllUsers = async (req, res) => {
 
 const getMyProfile = async (req, res) => {
   try {
-    const userId = req.user.id;
+    const { userId } = req.params;
 
-    const user = await User.findById(userId).lean();
+    const user = await User.findById(userId);
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
